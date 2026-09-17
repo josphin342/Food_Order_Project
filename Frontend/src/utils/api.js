c7cc7a42a4a60d674f107caa9af892f228004391
josphin342 <josphin800@gmail.com>
2026-09-17 23:29:22 +0530

    Updated Api Configuration

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -3,11 +3,11 @@
 import axios from "axios";
 import qs from "qs";
 
-const api=axios.create({
-    baseURL: "/api",
-    withCredentials: true,
-    paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }),
-})
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "/api",
+  withCredentials: true,
+  paramsSerializer: (params) =>
+    qs.stringify(params, { arrayFormat: "repeat" }),
+});
 
 export default api;
-
